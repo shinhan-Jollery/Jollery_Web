921bd6d8278d141414ace973278bf9ddc2571ba5
inetko2 <gkswhdgus98@gmail.com>
2024-12-18 12:46:18 +0900

    xml file repair

---
+++ src/main/java/com/shinhan/pay/PayController.java
@@ -0,0 +1,16 @@
+package com.shinhan.pay;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pcpay")
+public class PayController {
+	
+	@GetMapping("/tosspay.do")
+	public String tosspay() {
+		return "pay/tosspay.html";
+	}
+
+}
---
+++ src/main/java/com/shinhan/pay/PayService.java
@@ -0,0 +1,8 @@
+package com.shinhan.pay;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PayService {
+
+}
