c674bbd670226d168cc9344942c791f578deeff6
ParkSiBang <qkrtlgus2755@naver.com>
2024-12-23 15:03:19 +0900

    Initial project

---
+++ src/main/java/com/shinhan/VO/ArtsDTO.java
@@ -0,0 +1,38 @@
+package com.shinhan.VO;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
+// 17°³ Ä®·³ 
+public class ArtsDTO {
+	int art_id;
+	int member_id;
+	int category_id;
+	String art_title;
+	int art_price;
+	String art_description;
+	String art_status;
+	int art_like;
+	int art_view;
+	LocalDateTime art_post_date;
+	String art_size;
+	String art_thumbnail_image;
+	String art_hovering_image;
+	String art_image2;
+	String art_image3;
+	String art_image4;
+	String art_image5;
+}
---
+++ src/main/java/com/shinhan/test/ExhibitionDao.java
@@ -0,0 +1,25 @@
+package com.shinhan.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.shinhan.VO.ExhibitionsDTO;
+
+@Repository
+public class ExhibitionDao {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	String namespace = "com.shinhan.test.";
+
+    public void insertExhibition(ExhibitionsDTO exhibition) {
+    	sqlSession.insert(namespace + "insertExhibition", exhibition);
+    }
+    
+    public void sellectAllExhibition() {
+    	sqlSession.selectList(namespace + "selectAllExhibition");
+	}
+}
+
