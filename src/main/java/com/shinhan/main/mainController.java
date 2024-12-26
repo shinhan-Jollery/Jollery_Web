710eb91c9d96d6bbc57e2b4261085d2ed9f162f6
inetko2 <gkswhdgus98@gmail.com>
2024-12-26 17:20:51 +0900

    cookie null issue

--- src/main/java/com/shinhan/main/mainController.java
+++ src/main/java/com/shinhan/main/mainController.java
@@ -12,8 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class mainController {
 	@GetMapping("main.do")
 	public String mypage(HttpSession session,HttpServletRequest  request) {
+		try {
 		System.out.println("userID:  "+(int) request.getAttribute("userId"));
 		System.out.println("username"+(String) request.getAttribute("Member_name"));
+		}
+		catch(Exception e){
+			System.out.println("내가 만든 쿠키~");
+		}
+		
 		return "main/main";
 	}
 }
