50725fbdd055ea33bf35c7dbd24888a6b9fc0f85
inetko2 <gkswhdgus98@gmail.com>
2025-01-02 14:25:32 +0900

    send mail & verify before send pw

--- src/main/java/com/shinhan/auth/AuthService.java
+++ src/main/java/com/shinhan/auth/AuthService.java
@@ -1,16 +1,24 @@
 package com.shinhan.auth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shinhan.VO.MembersDTO;
+import com.shinhan.auth.mail.AuthMailService;
 
 
 @Service
 public class AuthService {
-
+	
+	@Autowired
+	AuthSignUpDAO authSignUpDAO;
     @Autowired
-    AuthSignUpDAO authSignUpDAO; 
+    AuthMemberDAO authMemberDAO; 
+    @Autowired
+    AuthMailService authMailService;
 
     public boolean addMember(MembersDTO member) {
         int result = authSignUpDAO.insertMember(member); 
@@ -35,5 +43,44 @@ public class AuthService {
         }
     }
     
+    public MembersDTO getMemberInfo(String member_login_ID) {
+    	
+    	
+        return authMemberDAO.getMemberByLoginId(member_login_ID);
+    }
+    /**
+     * login_ID랑 이름받고 검증 후 인증 mail 발송
+     * @param member_login_ID
+     * @param member_name
+     * @return json
+     */
+    public Map<String, Object> emailCheckerByLoginId_name(String member_login_ID, String member_name){
+    	MembersDTO member = getMemberInfo(member_login_ID);
+        if (member.getMember_name().equals(member_name)){
+        	return authMailService.sendEmail(member.getMember_email());
+        }
+    	Map<String, Object> response = new HashMap<>();
+    	response.put("message", "아이디와 Email이 일치하지 않습니다.");
+    	response.put("status", "error");
+    	return response;
+    }
+    
+    /**
+     * login_ID랑 이름받고 검증 후 코드 검증
+     * @param member_login_ID
+     * @param member_name
+     * @param code
+     * @return json
+     */
+    public Map<String, Object> verifyByLoginId_name(String member_login_ID, String member_name,String code){
+    	MembersDTO member = getMemberInfo(member_login_ID);
+        if (member.getMember_name().equals(member_name)){
+        	return authMailService.verifyCode(member.getMember_email(), code);
+        }
+    	Map<String, Object> response = new HashMap<>();
+    	response.put("message", "아이디와 Email이 일치하지 않습니다.");
+    	response.put("status", "error");
+    	return response;
+    }
 
 }
--- src/main/java/com/shinhan/auth/mail/AuthMailRestController.java
+++ src/main/java/com/shinhan/auth/mail/AuthMailRestController.java
@@ -1,35 +1,53 @@
 package com.shinhan.auth.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
+import com.shinhan.VO.MembersDTO;
+import com.shinhan.auth.AuthService;
+
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthMailRestController {
 
-    @Autowired
-    AuthMailService authMailService;
-    
-    // 이메일 인증 코드 발송
-    @PostMapping("/sendMemberEmail")
-    public Map<String, Object> sendMemberEmail(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
-        return authMailService.sendEmail(email);
-    }
-    // 이메일 인증 코드 발송
-    @PostMapping("/sendEmail")
-    public Map<String, Object> sendEmail(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
-        return authMailService.sendEmail(email);
-    }
+	@Autowired
+	AuthMailService authMailService;
+	@Autowired
+	AuthService authService;
 
-    // 인증 코드 확인
-    @PostMapping("/verifyCode")
-    public Map<String, Object> verifyCode(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
-        String code = request.get("code");
-        return authMailService.verifyCode(email, code);
-    }
+	// 이름과 아이디로 이메일 찾아서 인증 코드 발송
+	@PostMapping("/sendMemberEmail")
+	public Map<String, Object> sendMemberEmail(@RequestBody Map<String, String> request) {
+		String member_name = request.get("membername");
+		String member_login_ID = request.get("memberid");
+		return authService.emailCheckerByLoginId_name(member_login_ID, member_name);
+	}
+
+	// 이메일 인증 코드 발송
+	@PostMapping("/sendEmail")
+	public Map<String, Object> sendEmail(@RequestBody Map<String, String> request) {
+		String email = request.get("email");
+		return authMailService.sendEmail(email);
+	}
+
+	// 인증 코드 확인
+	@PostMapping("/verifyCode")
+	public Map<String, Object> verifyCode(@RequestBody Map<String, String> request) {
+		String email = request.get("email");
+		String code = request.get("code");
+		return authMailService.verifyCode(email, code);
+	}
+
+	// 인증 코드 확인
+	@PostMapping("/MemberverifyCode")
+	public Map<String, Object> MemberverifyCode(@RequestBody Map<String, String> request) {
+		String member_name = request.get("membername");
+		String member_login_ID = request.get("memberid");
+		String code = request.get("code");
+		return authService.verifyByLoginId_name(member_login_ID, member_name, code);
+	}
 }
