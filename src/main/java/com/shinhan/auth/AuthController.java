b98c35f4484a6fe2808f3d77a89ab1428679a30b
inetko2 <gkswhdgus98@gmail.com>
2025-01-02 16:27:22 +0900

    resolve conflicts

--- src/main/java/com/shinhan/auth/AuthController.java
+++ src/main/java/com/shinhan/auth/AuthController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
-	// �굹以묒뿉 mypage�뒗 �뵲濡� 鍮쇨퀬 �븘�꽣濡� 濡쒓렇�씤 �긽�깭�씤�뜲
-	// auth �젒洹� �븯硫� 留덉씠�럹�씠吏��굹 硫붿씤 �럹�씠吏�濡� �떎�뒘湲곌쾶 �빐�꽌 �씠以� 濡쒓렇�씤 留됯린
 	@GetMapping("mypage.do")
 	public String mypage() {
 		return "auth/mypage";
